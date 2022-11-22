604366e9d4f4ef198585475a64efd4117ac5678d
leaveitblank32 <leaveitblank32@gmail.com>
2022-11-22 20:59:39 +0530

    minor chanes

--- source/routes/index.ts
+++ source/routes/index.ts
@@ -3,7 +3,7 @@ import controller from '../controllers/example';
 const router = express.Router();
 const authJWT = require('../middleware/authJWT');
 
-
+router.get('/home', controller.test1);
 router.get('/test1', authJWT, controller.test1);
 router.get('/test2', authJWT, controller.test2);
 router.get('/fail1', authJWT, controller.fail1);
